96b8371289512f10860f2bb36357d56102ec6004
jeter5274 <jeter5274@gmail.com>
2020-12-02 10:09:22 +0900

    문제풀이 보고 보완

--- src/com/javaex/ex06/MyBase.java
+++ src/com/javaex/ex06/MyBase.java
@@ -3,7 +3,7 @@ package com.javaex.ex06;
 public class MyBase extends Base{
 
 	public void service(String state) {
-
+		/*
         if (state.equals("낮")) { //문자열의 같다(==) 는 equals()를 사용합니다.
             day();
         } else if(state.equals("밤")){
@@ -11,9 +11,19 @@ public class MyBase extends Base{
         } else {
         	System.out.println("오후도 낮과 마찬가지로 공부해야 합니다.");
         }
+        */
+		if (state.equals("오후")) { //문자열의 같다(==) 는 equals()를 사용합니다.
+            afternoon();
+        } else {
+        	super.service(state);
+        }
     }
     
 	public void day() {
         System.out.println("낮에는 열심히 수업듣자");
+    }
+	
+	public void afternoon() {
+		System.out.println("오후도 낮과 마찬가지로 공부해야 합니다.");
     }
 }
